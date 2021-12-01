06821a8b49de9b89a8a61a7872dd102306b067e5
Gloria Chan <changl3@oocl.com>
2021-12-01 16:26:17 +0800

    [java-oop][Gloria] -feat: AC2 commit

--- src/main/java/com/afs/oopractice/HelloWorld.java
+++ src/main/java/com/afs/oopractice/HelloWorld.java
@@ -4,5 +4,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         Car car = new Car("Car",30);
         car.speedUp();
+
+        Truck truck = new Truck("Truck", 30);
+        truck.speedUp();
     }
 }
